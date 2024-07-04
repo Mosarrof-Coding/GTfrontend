73847032cbfbed04bd413c51e87aaffdcd9ff748
Mosarrof-Coding <tinaberry112188@gmail.com>
2024-07-04 14:06:50 +0600

    Add: loader css

--- src/pages/ServicePage.jsx
+++ src/pages/ServicePage.jsx
@@ -1,21 +1,9 @@
-/* eslint-disable react/no-unescaped-entities */
 import PriceCard from "../components/Service/PriceCard";
 
 export default function ServicePage() {
   return (
-    <section className="section">
-      <div className="container">
-        <div className="heading text-center max-w-[560px] mx-auto pb-8">
-          <h2 className="text-2xl font-semibold my-2">Our Services</h2>
-          <h3 className="text-xl font-medium text-gray-600">
-            Dive into our membership tiers designed to match your golfing
-            lifestyle. Whether you're looking for casual access to fully commit.
-          </h3>
-        </div>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-stretch lg:grid-cols-3 md:gap-8">
-          <PriceCard />
-        </div>
-      </div>
-    </section>
+    <>
+      <PriceCard />
+    </>
   );
 }
