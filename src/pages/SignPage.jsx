7bf23c5ec703aa1fc10ddd438fcabef1f19c43d4
Mosarrof-Coding <tinaberry112188@gmail.com>
2024-07-06 02:21:37 +0600

    Add: signup and login fn to backend

--- src/pages/SignPage.jsx
+++ src/pages/SignPage.jsx
@@ -1,51 +1,105 @@
 /* eslint-disable react/no-unescaped-entities */
+import axios from "axios";
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function SignPage() {
+  const [eye, setEye] = useState(false);
+  const eyeChange = () => {
+    setEye(!eye);
+  };
+
+  // form data
+  const [user, setUser] = useState({ name: "", email: "", password: "" });
+
+  const handleUser = ({ target }) => {
+    const { name, value } = target;
+    setUser((currentUser) => ({
+      ...currentUser,
+      [name]: value,
+    }));
+  };
+
+  // data post to strapi
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const url = `http://localhost:1337/api/auth/local/register`;
+      if (user.name && user.email && user.password) {
+        const res = await axios.post(url, {
+          username: user.name,
+          email: user.email,
+          password: user.password,
+        });
+        toast.success("Account created successfully!");
+        console.log("post", res);
+      } else {
+        toast.error("All fields are required!");
+      }
+    } catch (error) {
+      toast.error("This didn't work. Error: " + error.message);
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <section className="section">
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          className: "",
+          duration: 5000,
+          style: {
+            background: "#363636",
+            color: "#fff",
+          },
+          success: {
+            duration: 3000,
+            theme: {
+              primary: "green",
+              secondary: "black",
+            },
+          },
+        }}
+      />
       <div className="container">
         <div className="flex items-center justify-center lg:col-span-7 xl:col-span-6">
           <div className="max-w-xl lg:max-w-3xl">
             <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
               Create an account
             </h1>
-            <form action="#" className="mt-8 grid grid-cols-6 gap-6">
-              <div className="col-span-6 sm:col-span-3">
+            <form
+              action="#"
+              className="mt-8 grid grid-cols-6 gap-6"
+              onSubmit={handleSignUp}
+            >
+              <div className="col-span-6">
                 <label
-                  htmlFor="FirstName"
+                  htmlFor="name"
                   className="block text-sm font-medium text-gray-700"
                 >
-                  First Name
+                  Name
                 </label>
 
                 <input
                   type="text"
-                  id="FirstName"
-                  name="first_name"
-                  className="mt-1 py-2 px-4 w-full rounded-md border focus:outline-blue-200 bg-white text-gray-700 shadow-sm capitalize"
-                />
-              </div>
-
-              <div className="col-span-6 sm:col-span-3">
-                <label
-                  htmlFor="LastName"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Last Name
-                </label>
-
-                <input
-                  type="text"
-                  id="LastName"
-                  name="last_name"
-                  className="mt-1 py-2 px-4 w-full rounded-md border focus:outline-blue-200 bg-white text-gray-700 shadow-sm capitalize"
+                  id="name"
+                  name="name"
+                  className="mt-1 py-2 px-4 w-full rounded-md border focus:outline-blue-200 bg-white text-gray-700 shadow-sm"
+                  value={user.name}
+                  onChange={handleUser}
                 />
               </div>
 
               <div className="col-span-6">
                 <label
-                  htmlFor="Email"
+                  htmlFor="email"
                   className="block text-sm font-medium text-gray-700"
                 >
                   Email{" "}
@@ -53,55 +107,53 @@ export default function SignPage() {
 
                 <input
                   type="email"
-                  id="Email"
+                  id="email"
                   name="email"
                   className="mt-1 py-2 px-4 w-full rounded-md border focus:outline-blue-200 bg-white text-gray-700 shadow-sm"
-                />
-              </div>
-
-              <div className="col-span-6 sm:col-span-3">
-                <label
-                  htmlFor="Password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Password{" "}
-                </label>
-
-                <input
-                  type="password"
-                  id="Password"
-                  name="password"
-                  className="mt-1 py-2 px-4 w-full rounded-md border focus:outline-blue-200 bg-white text-gray-700 shadow-sm"
-                />
-              </div>
-
-              <div className="col-span-6 sm:col-span-3">
-                <label
-                  htmlFor="PasswordConfirmation"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Password Confirmation
-                </label>
-
-                <input
-                  type="password"
-                  id="PasswordConfirmation"
-                  name="password_confirmation"
-                  className="mt-1 py-2 px-4 w-full rounded-md border focus:outline-blue-200 bg-white text-gray-700 shadow-sm"
+                  value={user.email}
+                  onChange={handleUser}
                 />
               </div>
 
               <div className="col-span-6">
-                <label htmlFor="MarketingAccept" className="flex gap-4">
+                <label
+                  htmlFor="password"
+                  className="block mb-1 text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <div className="relative">
+                  <input
+                    type={eye ? "text" : "password"}
+                    id="password"
+                    name="password"
+                    placeholder="Type your password"
+                    className="py-2 px-4 w-full rounded-md border focus:outline-blue-200 bg-white text-gray-700 shadow-sm"
+                    value={user.password}
+                    onChange={handleUser}
+                  />
+                  <div className="absolute right-0 top-0 min-h-full w-6 flex items-center">
+                    <span className="view cursor-pointer" onClick={eyeChange}>
+                      {eye ? (
+                        <FaRegEyeSlash size={18} />
+                      ) : (
+                        <FaRegEye size={18} />
+                      )}
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-span-6">
+                <label htmlFor="marketingAccept" className="flex gap-4">
                   <input
                     type="checkbox"
-                    id="MarketingAccept"
+                    id="marketingAccept"
                     name="marketing_accept"
                     className="size-5 rounded-md bg-white shadow-sm"
                   />
-
                   <span className="text-sm text-gray-700">
-                    I want to receive emails about events, product updates and
+                    I want to receive emails about events, product updates, and
                     company announcements.
                   </span>
                 </label>
@@ -128,7 +180,7 @@ export default function SignPage() {
 
                 <p className="mt-4 text-sm text-gray-500 sm:mt-0">
                   Already have an account?
-                  <Link to={"/login"} className="text-gray-700 underline">
+                  <Link to="/login" className="text-gray-700 underline">
                     Log in
                   </Link>
                 </p>
