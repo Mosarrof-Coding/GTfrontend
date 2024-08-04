151dcea4aa02fcf24d292eaf679411c5ba724bfc
Mosarrof-Coding <tinaberry112188@gmail.com>
2024-08-04 12:43:35 +0600

    Add: transfer h3

--- src/components/Blog/BlogCard.jsx
+++ src/components/Blog/BlogCard.jsx
@@ -5,9 +5,6 @@ export default function BlogCard({ blog }) {
   return (
     <>
       <article className="h-full overflow-hidden rounded-lg shadow transition hover:shadow-lg">
-        <h3 className="px-4 sm:p-6 text-lg text-gray-900 hover:underline ">
-          {blog.attributes.Title}
-        </h3>
         <div>
           <img
             src={blog.attributes.image.data.attributes.url}
@@ -16,6 +13,9 @@ export default function BlogCard({ blog }) {
           />
         </div>
         <div className="bg-white px-4 sm:p-6">
+          <h3 className="my-1 font-medium text-lg text-gray-900">
+            {blog.attributes.Title}
+          </h3>
           <h4 className="text-lg font-semibold text-purple-600">
             {blog.attributes.Category}
           </h4>
