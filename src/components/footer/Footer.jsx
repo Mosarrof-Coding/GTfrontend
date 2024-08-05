f227c0f1846971952c918a7f2da12936161bbd45
Mosarrof-Coding <tinaberry112188@gmail.com>
2024-08-05 22:21:38 +0600

    Add: footerize

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,21 @@
-import { Link } from "react-router-dom";
-
 /* eslint-disable react/no-unescaped-entities */
+import { Link } from "react-router-dom";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+} from "react-icons/fa";
+
 export default function Footer() {
   return (
     <>
       <footer className="w-full text-slate-500">
         <div className="border-t border-slate-200 bg-slate-100 py-12 text-base">
-          <div className="container">
-            <div className="grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12">
+          <div className="container mx-auto px-6">
+            <div className="grid grid-cols-2 gap-6 md:grid-cols-8 lg:grid-cols-12">
               <div
-                className="col-span-4 md:col-span-8 lg:col-span-4"
+                className="col-span-2 md:col-span-8 lg:col-span-4"
                 aria-labelledby="footer-header"
               >
                 <Link
@@ -18,123 +24,26 @@ export default function Footer() {
                 >
                   <span className="pb-6 inline-block">G🏐LF</span>
                 </Link>
-                <p className="hidden md:block">
-                  Expertly made, responsive, accessible components in React and
-                  HTML ready to be used on your website or app. Just copy and
-                  paste them on your Tailwind CSS project.
+                <p className="hidden md:block text-sm lg:text-base">
+                  Discover premium golf experiences and connect with fellow golf
+                  enthusiasts. Enhance your game with expert tips and exclusive
+                  membership benefits.
                 </p>
-                <p className="block md:hidden">
-                  Expertly made, responsive, accessible components in React and
-                  HTML ready to be used on your website or app.
+                <p className="block md:hidden text-sm">
+                  Discover premium golf experiences and connect with fellow golf
+                  enthusiasts.
                 </p>
               </div>
+
               <nav
-                className="col-span-2 md:col-span-4 lg:col-span-2 hidden md:block"
-                aria-labelledby="footer-product-5-logo"
+                className="col-span-1 md:col-span-4 lg:col-span-2 hidden md:block"
+                aria-labelledby="footer-about"
               >
                 <h3
                   className="mb-6 text-base font-medium text-slate-700"
-                  id="footer-product-5-logo"
+                  id="footer-about"
                 >
-                  Product
-                </h3>
-                <ul>
-                  <li className="mb-2 leading-6">
-                    <Link
-                      to="/service"
-                      className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
-                    >
-                      Service
-                    </Link>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <Link
-                      to="/customers"
-                      className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
-                    >
-                      Customers
-                    </Link>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <Link
-                      to="/why-us"
-                      className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
-                    >
-                      Why us?
-                    </Link>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <Link
-                      to="/pricing"
-                      className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
-                    >
-                      Pricing
-                    </Link>
-                  </li>
-                </ul>
-              </nav>
-              <nav
-                className="col-span-2 md:col-span-4 lg:col-span-2 hidden md:block"
-                aria-labelledby="footer-docs-5-logo"
-              >
-                <h3
-                  className="mb-6 text-base font-medium text-slate-700"
-                  id="footer-docs-5-logo"
-                >
-                  Docs & Help
-                </h3>
-                <ul>
-                  <li className="mb-2 leading-6">
-                    <Link
-                      to="/documentation"
-                      className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
-                    >
-                      Documentation
-                    </Link>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <Link
-                      to="/training"
-                      className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
-                    >
-                      Training
-                    </Link>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <Link
-                      to="/system-status"
-                      className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
-                    >
-                      System status
-                    </Link>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <Link
-                      to="/faqs"
-                      className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
-                    >
-                      FAQ's
-                    </Link>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <Link
-                      to="/help-center"
-                      className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
-                    >
-                      Help center
-                    </Link>
-                  </li>
-                </ul>
-              </nav>
-              <nav
-                className="col-span-2 md:col-span-4 lg:col-span-2"
-                aria-labelledby="footer-company-5-logo"
-              >
-                <h3
-                  className="mb-6 text-base font-medium text-slate-700"
-                  id="footer-company-5-logo"
-                >
-                  Company
+                  About
                 </h3>
                 <ul>
                   <li className="mb-2 leading-6">
@@ -142,15 +51,53 @@ export default function Footer() {
                       to="/about"
                       className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
                     >
-                      About us
+                      About Us
                     </Link>
                   </li>
                   <li className="mb-2 leading-6">
                     <Link
-                      to="/blog"
+                      to="/our-team"
                       className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
                     >
-                      Blog
+                      Our Team
+                    </Link>
+                  </li>
+                  <li className="mb-2 leading-6">
+                    <Link
+                      to="/training-schedule"
+                      className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
+                    >
+                      Training Schedule
+                    </Link>
+                  </li>
+                  <li className="mb-2 leading-6">
+                    <Link
+                      to="/events"
+                      className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
+                    >
+                      Events
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+
+              <nav
+                className="col-span-1 md:col-span-4 lg:col-span-2 hidden md:block"
+                aria-labelledby="footer-services"
+              >
+                <h3
+                  className="mb-6 text-base font-medium text-slate-700"
+                  id="footer-services"
+                >
+                  Services
+                </h3>
+                <ul>
+                  <li className="mb-2 leading-6">
+                    <Link
+                      to="/service"
+                      className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
+                    >
+                      Online Reservation
                     </Link>
                   </li>
                   <li className="mb-2 leading-6">
@@ -166,66 +113,103 @@ export default function Footer() {
                       to="/contact"
                       className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
                     >
-                      Contact
+                      Contact Us
                     </Link>
                   </li>
                 </ul>
               </nav>
+
               <nav
-                className="col-span-2 md:col-span-4 lg:col-span-2"
-                aria-labelledby="footer-legal-5-logo"
+                className="col-span-1 md:col-span-4 lg:col-span-2 hidden md:block"
+                aria-labelledby="footer-resources"
               >
                 <h3
                   className="mb-6 text-base font-medium text-slate-700"
-                  id="footer-legal-5-logo"
+                  id="footer-resources"
                 >
-                  Legal
+                  Resources
                 </h3>
                 <ul>
                   <li className="mb-2 leading-6">
                     <Link
-                      to="/terms"
+                      to="/blog"
                       className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
                     >
-                      Terms
+                      Blog
                     </Link>
                   </li>
                   <li className="mb-2 leading-6">
                     <Link
-                      to="/privacy"
+                      to="/faq"
                       className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
                     >
-                      Privacy
+                      FAQ's
                     </Link>
                   </li>
                   <li className="mb-2 leading-6">
                     <Link
-                      to="/cookies"
+                      to="/gallery"
                       className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
                     >
-                      Cookies
-                    </Link>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <Link
-                      to="/licenses"
-                      className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
-                    >
-                      Licenses
-                    </Link>
-                  </li>
-                  <li className="mb-2 leading-6">
-                    <Link
-                      to="/settings"
-                      className="transition-colors duration-300 hover:text-emerald-500 focus:text-emerald-600"
-                    >
-                      Settings
+                      Our Gallery
                     </Link>
                   </li>
                 </ul>
               </nav>
+
+              <nav
+                className="col-span-1 md:col-span-4 lg:col-span-2"
+                aria-labelledby="footer-social"
+              >
+                <h3
+                  className="mb-6 text-base font-medium text-slate-700"
+                  id="footer-social"
+                >
+                  Follow Us
+                </h3>
+                <div className="flex space-x-4">
+                  <a
+                    href="https://facebook.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-slate-700 hover:text-emerald-500"
+                  >
+                    <FaFacebookF size={24} />
+                  </a>
+                  <a
+                    href="https://twitter.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-slate-700 hover:text-emerald-500"
+                  >
+                    <FaTwitter size={24} />
+                  </a>
+                  <a
+                    href="https://instagram.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-slate-700 hover:text-emerald-500"
+                  >
+                    <FaInstagram size={24} />
+                  </a>
+                  <a
+                    href="https://linkedin.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-slate-700 hover:text-emerald-500"
+                  >
+                    <FaLinkedinIn size={24} />
+                  </a>
+                </div>
+              </nav>
             </div>
           </div>
+        </div>
+        <div className="bg-slate-200 py-4 text-center text-sm text-slate-500">
+          <p>
+            &copy; {new Date().getFullYear()} G🏐LF. All Rights Reserved by
+            EnvyTheme.
+          </p>
         </div>
       </footer>
     </>
