7a71bbc5aa2481943ee4e664e82c970e84b56c98
Mosarrof-Coding <tinaberry112188@gmail.com>
2024-08-28 23:03:52 +0600

    Access:leading

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -8,13 +8,13 @@ export default function Hero() {
       <div className="absolute left-0 top-0 right-0 bottom-0 grid place-items-center px-[15px]">
         <div className="mx-auto max-w-[800px] text-center flex flex-col justify-between gap-2 md:flex-none py-4">
           <div>
-            <h2 className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-4xl font-semibold  text-white">
+            <h2 className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-4xl font-semibold  text-white leading-none">
               Welcome to GalfTimes
             </h2>
             <h1 className="font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-blue-300 block md:mt-2 lg:mt-3">
               Elevate Your Game with Premium
             </h1>
-            <p className="mt-1 sm:mt-2 lg:mt-4 lg:px-16 text-gray-200 text-sm md:text-lg lg:text-xl font-normal">
+            <p className="mt-1 sm:mt-2 lg:mt-4 lg:px-16 text-gray-200 text-sm md:text-lg lg:text-xl font-normal leading-[1] md:leading-normal tracking-tight">
               Discover top-quality clubs, apparel, and accessories designed to
               enhance every swing. Join the community of golfers who trust us
               for their journey from tee to green.
@@ -22,7 +22,7 @@ export default function Hero() {
           </div>
           <Link
             to=""
-            className="w-fit mx-auto mt-8 sm:mt-12 md:mt-16 rounded bg-red-600 px-4 md:px-6 lg:px-8 py-1 md:pt-2 lg:py-3 text-xs lg:text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 leading-none"
+            className="w-fit mx-auto mt-4 sm:mt-12 md:mt-16 rounded bg-red-600 px-4 md:px-6 lg:px-8 py-1 md:pt-2 lg:py-3 text-xs lg:text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 leading-none"
           >
             SHOPE NOW
           </Link>
